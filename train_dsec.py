89dd5e0bd8f6d416bc3745421dc6cfda187216c9
Alexander Gaul <alexander.gaul@dlr.de>
2022-03-04 11:36:01 +0100

    Training: refactored training into separate class.

--- train_dsec.py
+++ train_dsec.py
@@ -35,6 +35,7 @@ parser.add_argument("--output_path", type=str)
 parser.add_argument("--message")
 parser.add_argument("--checkpoint", type=str)
 parser.add_argument("--config_file", type=open)
+parser.add_argument("--resume", action='store_true')
 
 
 """parser.add_argument("--train_seqs", nargs='+', type=int, default=list(range(1, 18)))
@@ -103,13 +104,6 @@ def main() :
     if args.config_file is not None :
         config = json.load(args.config_file)
 
-
-
-    save_figures_loss = False
-
-    #vis_train_frames = list(zip(*([iter(args.vis_train_frames)] * 2)))
-    #vis_val_frames = list(zip(*([iter(args.vis_val_frames)] * 2)))
-
     if hasattr(args, 'output_path') and args.output_path is not None :
         save_figures_loss = True
         output_path = Path(args.output_path)
@@ -117,25 +111,29 @@ def main() :
 
         if not os.path.exists(output_path):
             os.makedirs(output_path)
+            with open(output_path / "settings.txt", 'w') as file :
+                file.write("\n".join("{}: {}".format(k, v) for k, v in vars(args).items()))
 
-        with open(output_path / "settings.txt", 'w') as file :
-            file.write("\n".join("{}: {}".format(k, v) for k, v in vars(args).items()))
+            with open(output_path / "config.json", 'w') as file :
+                file.write(json.dumps(config, indent=4))
 
-        with open(output_path / "config.json", 'w') as file :
-            file.write(json.dumps(config, indent=4))
+            args.resume = False
+
+        elif args.resume :
+            existing_files = sorted(os.listdir(output_path))
+            if "config.json" in existing_files :
+                print("Resuming config: " + str(output_path / "config.json"))
+                config = json.load(open(output_path / "config.json"))
+            checkpoints = [f for f in existing_files if "checkpoint" in f]
+            latest = np.argmax([int(f.split("checkpoint")[1]) for f in checkpoints])
+            print("Resuming checkpoint: " + str(output_path / checkpoints[latest]))
+            args.checkpoint = output_path / checkpoints[latest]
+            # TODO: how to find latest config file
 
 
     print(json.dumps(config, indent=4))
 
-    
-
     epochs = config['training']['epochs']
-    lr = config['training']['lr']
-
-    print_freq = 1
-    val_freq = 2
-    checkpoint_freq = config['output']['checkpoint_freq']
-
 
     torch.backends.cudnn.benchmark = True
     print("Benchmark: " + str(torch.backends.cudnn.benchmark))
@@ -143,163 +141,27 @@ def main() :
     print(device)
 
 
-    train_set = DSEC(**config['train_set'])
-
-    # TODO: mulitple validation sets
-    val_sets = [DSEC(**config['val_sets'][i])
-                for i in range(len(config['val_sets']))]
-
-
-    print("Training set length: " + str(len(train_set)))
-    for val_set in val_sets :
-        print("Validation set length: " + str(len(val_set)))
-
-    train_loader = torch.utils.data.DataLoader(train_set,
-                                               collate_fn=collate_dict_list,
-                                               shuffle=True,
-                                               pin_memory=True,
-                                               **config['data_loader'])
-    batch_size = train_loader.batch_size
-
     model = EventTransformer(**config['model'])
     model.to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr)
-    warm_up_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer,
-                                                          start_factor=config['training'].get('warm_up_init', 1.),
-                                                          total_iters=config['training'].get('warm_up_length', 0))
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=config['training']['lr_halflife'], gamma=0.5)
-
-    train_loss_history = []
-    val_loss_histories = [[] for _ in val_sets]
-    val_loss_steps = []
     epoch_0 = 0
 
-    if args.checkpoint is not None :
-        checkpoint = torch.load(args.checkpoint)
-        epoch_0 = checkpoint['epoch']
-        model.load_state_dict(checkpoint['model'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-        scheduler.load_state_dict(checkpoint['scheduler'])
-        train_loss_history = checkpoint['train_loss_history']
-        val_loss_histories = checkpoint['val_loss_histories']
-        val_loss_steps = checkpoint['val_loss_steps']
-        random.setstate(checkpoint['py_rng'])
-        torch.set_rng_state(checkpoint['torch_rng'])
-
     LFunc = torch.nn.L1Loss()
-    print(type(LFunc))
 
+    training = Training(model, DSEC,
+                        forward_perceiver, eval_perceiver_out, LFunc,
+                        config, device,
+                        output_path,
+                        collate_dict_list)
 
-    writer = tensorboard.SummaryWriter(log_dir=output_path, purge_step=epoch_0)
+    if args.checkpoint :
+        print("Loading checkpoint: " + str(args.checkpoint))
+        training.load_checkpoint(args.checkpoint)
 
     for it in range(epoch_0, epochs) :
-        epoch_begin = time.time()
-        if it == args.finetune_epoch :
-            train_set.set_crop()
-            if args.finetune_lr :
-                optimizer = torch.optim.Adam(model.parameters(), args.finetune_lr)
-                warm_up_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer,
-                                                                      start_factor=config['training'].get(
-                                                                          'warm_up_init', 1.),
-                                                                      total_iters=config['training'].get(
-                                                                          'warm_up_length', 0))
-                scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_halflife, gamma=0.5)
-                warm_up_scheduler.step(it-1)
-                scheduler.step(it-1)
-            if args.finetune_batch_size :
-                train_loader = torch.utils.data.DataLoader(train_set,
-                                                           batch_size=args.finetune_batch_size,
-                                                           collate_fn=collate_dict_list,
-                                                           num_workers=args.num_workers, shuffle=True,
-                                                           pin_memory=True)
-            train_set.random_backward = False
-
-        report = process_epoch(it, model, LFunc, train_set, device,
-                               forward_perceiver, eval_perceiver_out,
-                               train_loader, optimizer,
-                               config['output']['vis_train_frames'] if it % config['output']['vis_freq'] == 0 else [],
-                               batch_size=batch_size if not train_set.batch_backward else batch_size * 2,
-                               full_query=config['training'].get('full_query', False),
-                               predict_targets=config['training'].get('predict_targets', False))
-        # print(report['runtime'])
-        warm_up_scheduler.step()
-        scheduler.step()
-
-        write_stats_to_tensorboard(report['stats'], writer, it, prefix="train")
-        train_loss_history.append(report['stats']['loss'])
-
-        # im_grid = torchvision.utils.make_grid([torch.tensor(im.transpose((2, 0,1 )))
-        #                                        for im in stats['ims'].values()])
-        for im_name in report['ims'].keys() :
-            writer.add_image("train/" + im_name,
-                             report['ims'][im_name].transpose((2, 0, 1)), it)
-
-        if it % print_freq == 0 :
-            it_string = ("Iteration: " + str(it) + ", training loss : " +
-                  "{0:.3f}".format(report['stats']['loss']) +
-                  ", time: " + "{0:.1f}".format(time.time() - epoch_begin))
-            print(it_string)
-            with open(output_path / "output.txt", 'w') as file:
-                file.write(it_string)
-
-
-        if it % val_freq == 0 or it % config['output']['vis_freq'] == 0 :
-            model.eval()
-
-            for idx_val_set, val_set in enumerate(val_sets) :
-                with torch.no_grad() :
-                    report = process_epoch(it, model, LFunc, val_set, device,
-                                           forward_perceiver, eval_perceiver_out,
-                                           vis_frames=config['output']['vis_val_frames']
-                                           if it % config['output']['vis_freq'] == 0 else [],
-                                           full_query=config['training'].get('full_query', False),
-                                           predict_targets=config['training'].get('predict_targets', False))
-
-                    # TODO: print frame
-                print("Iteration: " + str(it) + ", validation loss : " +
-                      "{0:.3f}".format(report['stats']['loss']))
-                val_loss_histories[idx_val_set].append(report['stats']['loss'])
-
-                write_stats_to_tensorboard(report['stats'], writer, it, prefix="val" + str(idx_val_set))
-
-                for im_name in report['ims'].keys():
-                    writer.add_image("val" + str(idx_val_set) + "/" + im_name,
-                                     report['ims'][im_name].transpose((2, 0, 1)), it)
-
-            val_loss_steps.append(it)
-
-            if save_figures_loss :
-                plt.plot(np.arange(0, len(train_loss_history)), train_loss_history, label="train")
-                for i, val_loss_history in enumerate(val_loss_histories) :
-                    if len(val_loss_history) == len(val_loss_steps):
-                        plt.plot(val_loss_steps, val_loss_history, label="val"+str(i))
-                plt.legend(loc='upper right')
-                plt.savefig(output_path / "loss_history.png",
-                            pad_inches=0.)
-                plt.close()
-
-            model.train()
-
-        if it % checkpoint_freq == 0 :
-            checkpoint = {
-                'epoch' : it + 1,
-                'model' : model.state_dict(),
-                'optimizer' : optimizer.state_dict(),
-                'scheduler' : scheduler.state_dict(),
-                'warmup' : warm_up_scheduler.state_dict(),
-                'train_loss_history' : train_loss_history,
-                'val_loss_histories' : val_loss_histories,
-                'val_loss_steps' : val_loss_steps,
-                'py_rng' : random.getstate(),
-                'torch_rng' : torch.get_rng_state()
-            }
-            torch.save(checkpoint, output_path / ("checkpoint" + str(it)))
+        training.step()
 
     print(torch.cuda.max_memory_allocated(device=device))
-
-    writer.flush()
-    writer.close()
 
 if __name__ == "__main__" :
     main()
--- training.py
+++ training.py
@@ -5,6 +5,7 @@ from torch.utils import tensorboard
 from torch.utils.data import DataLoader
 
 import time
+import random
 
 from utils import default, collate_dict_list, nested_to_device
 from plot import *
@@ -158,11 +159,215 @@ def process_epoch(epoch, model, LFunc, dataset, device,
         #report['runtime']['model'] += fwd_call_ev[0].elapsed_time(fwd_call_ev[1])
         report['runtime']['loss'] += loss_begin.elapsed_time(loss_end)
         report['runtime']['backward'] += backward_begin.elapsed_time(backward_end)
+        report['stats']['loss'] += loss.item() / len(dataset) * batch_size
         report['runtime']['total'] += time.time() - time_begin
 
-        report['stats']['loss'] += loss.item() / len(dataset) * batch_size
-
     return report
+
+
+def config_add_defaults(config) :
+    config['training'].setdefault('params', {})
+
+    config['data_loader'].setdefault('shuffle', True)
+    config['data_loader'].setdefault('pin_memory', True)
+
+    config['output'].setdefault('checkpoint_freq', 1000)
+
+    return config
+
+
+class Training :
+    def __init__(self, model, DataSetClass, forward_model_func, eval_model_func, loss_func,
+                 config, device, output_path, collate_fn=None) :
+        self.config = config_add_defaults(config)
+
+
+        self.model = model
+
+        self.train_set = DataSetClass(**self.config['train_set'])
+
+        self.val_sets = [DataSetClass(**self.config['val_sets'][i])
+                         for i in range(len(config['val_sets']))]
+
+        print("Training set length: " + str(len(self.train_set)))
+        for val_set in self.val_sets:
+            print("Validation set length: " + str(len(val_set)))
+
+        self.forward_model_func = forward_model_func
+        self.eval_model_func = eval_model_func
+        self.loss_func = loss_func
+
+        self.device = device
+
+        self.output_path = output_path
+
+        self.train_loader = torch.utils.data.DataLoader(self.train_set,
+                                                        collate_fn=collate_fn,
+                                                        **self.config['data_loader'])
+
+        self.optimizer = torch.optim.Adam(model.parameters(), config['training']['lr'])
+
+        self.schedulers = []
+        if 'lr_halflife' in config['training'] :
+            self.schedulers.append(
+                torch.optim.lr_scheduler.StepLR(
+                    self.optimizer,
+                    step_size=config['training']['lr_halflife'],
+                    gamma=0.5))
+        if 'warm_up' in config['training'] :
+            self.schedulers.append(
+                torch.optim.lr_scheduler.LinearLR(
+                    self.optimizer,
+                    start_factor=config['training']['warm_up']['init'],
+                    total_iters=config['training']['warm_up']['length']))
+
+        self.epoch = 0
+
+        self.batch_size = self.train_loader.batch_size
+        # TOOD: streamline this
+        if type(self.train_set[0]) is list :
+            self.batch_size *= len(self.train_set[0])
+
+        self.writer = tensorboard.SummaryWriter(log_dir=output_path) #, purge_step=epoch_0)
+
+        self.train_loss_history = []
+        self.val_loss_histories = [[] for _ in self.val_sets]
+        self.val_loss_steps = []
+
+        self.val_freq = 2
+
+
+    def __del__(self) :
+        self.writer.flush()
+        self.writer.close()
+
+    def step(self) :
+        epoch_begin = time.time()
+        """if it == args.finetune_epoch :
+            train_set.set_crop()
+            if args.finetune_lr :
+                optimizer = torch.optim.Adam(model.parameters(), args.finetune_lr)
+                warm_up_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer,
+                                                                      start_factor=config['training'].get(
+                                                                          'warm_up_init', 1.),
+                                                                      total_iters=config['training'].get(
+                                                                          'warm_up_length', 0))
+                scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_halflife, gamma=0.5)
+                warm_up_scheduler.step(it-1)
+                scheduler.step(it-1)
+            if args.finetune_batch_size :
+                train_loader = torch.utils.data.DataLoader(train_set,
+                                                           batch_size=args.finetune_batch_size,
+                                                           collate_fn=collate_dict_list,
+                                                           num_workers=args.num_workers, shuffle=True,
+                                                           pin_memory=True)
+            train_set.random_backward = False"""
+
+        report = process_epoch(self.epoch, self.model, self.loss_func, self.train_set,
+                               self.device,
+                               self.forward_model_func, self.eval_model_func,
+                               self.train_loader, self.optimizer,
+                               self.config['output']['vis_train_frames'] if self.epoch % self.config['output']['vis_freq'] == 0 else [],
+                               batch_size=self.batch_size,
+                               **self.config['training']['params'])
+
+        for scheduler in self.schedulers :
+            scheduler.step()
+
+        write_stats_to_tensorboard(report['stats'], self.writer, self.epoch, prefix="train")
+        self.train_loss_history.append(report['stats']['loss'])
+
+        # im_grid = torchvision.utils.make_grid([torch.tensor(im.transpose((2, 0,1 )))
+        #                                        for im in stats['ims'].values()])
+        for im_name in report['ims'].keys() :
+            self.writer.add_image("train/" + im_name,
+                                  report['ims'][im_name].transpose((2, 0, 1)), self.epoch)
+
+        it_string = ("Iteration: " + str(self.epoch) + ", training loss : " +
+              "{0:.3f}".format(report['stats']['loss']) +
+              ", time: " + "{0:.1f}".format(time.time() - epoch_begin))
+        print(it_string)
+        with open(self.output_path / "output.txt", 'w') as file:
+            file.write(it_string)
+
+
+        if self.epoch % self.val_freq == 0 or self.epoch % self.config['output']['vis_freq'] == 0 :
+            self.model.eval()
+
+            for idx_val_set, val_set in enumerate(self.val_sets) :
+                with torch.no_grad() :
+                    report = process_epoch(self.epoch, self.model, self.loss_func, val_set,
+                                           self.device,
+                                           self.forward_model_func, self.eval_model_func,
+                                           vis_frames=self.config['output']['vis_val_frames']
+                                           if self.epoch % self.config['output']['vis_freq'] == 0 else [],
+                                           **self.config['training']['params'])
+
+                    # TODO: print frame
+                print("Iteration: " + str(self.epoch) + ", validation loss : " +
+                      "{0:.3f}".format(report['stats']['loss']))
+                self.val_loss_histories[idx_val_set].append(report['stats']['loss'])
+
+                write_stats_to_tensorboard(report['stats'], self.writer, self.epoch, prefix="val" + str(idx_val_set))
+
+                for im_name in report['ims'].keys():
+                    self.writer.add_image("val" + str(idx_val_set) + "/" + im_name,
+                                     report['ims'][im_name].transpose((2, 0, 1)), self.epoch)
+
+            self.val_loss_steps.append(self.epoch)
+
+
+            """if save_figures_loss :
+                plt.plot(np.arange(0, len(train_loss_history)), train_loss_history, label="train")
+                for i, val_loss_history in enumerate(val_loss_histories) :
+                    if len(val_loss_history) == len(val_loss_steps):
+                        plt.plot(val_loss_steps, val_loss_history, label="val"+str(i))
+                plt.legend(loc='upper right')
+                plt.savefig(output_path / "loss_history.png",
+                            pad_inches=0.)
+                plt.close()"""
+
+            self.model.train()
+
+        self.epoch += 1
+
+        if self.epoch % self.config['output']['checkpoint_freq'] == 0 :
+            self.write_checkpoint()
+
+
+    def set_finetune(self) :
+        pass
+
+
+    def load_checkpoint(self, path) :
+        checkpoint = torch.load(path)
+        self.epoch = checkpoint['epoch']
+        self.model.load_state_dict(checkpoint['model'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        for i, scheduler in enumerate(self.schedulers) :
+            scheduler.load_state_dict(checkpoint['scheduler'][i])
+        self.train_loss_history = checkpoint['train_loss_history']
+        self.val_loss_histories = checkpoint['val_loss_histories']
+        self.val_loss_steps = checkpoint['val_loss_steps']
+        random.setstate(checkpoint['py_rng'])
+        torch.set_rng_state(checkpoint['torch_rng'])
+
+
+    def write_checkpoint(self) :
+        checkpoint = {
+            'epoch': self.epoch,
+            'model': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'scheduler': [scheduler.state_dict()
+                          for scheduler in self.schedulers],
+            'train_loss_history': self.train_loss_history,
+            'val_loss_histories': self.val_loss_histories,
+            'val_loss_steps': self.val_loss_steps,
+            'py_rng': random.getstate(),
+            'torch_rng': torch.get_rng_state()
+        }
+        torch.save(checkpoint, self.output_path / ("checkpoint" + str(self.epoch)))
+
 
 
 def write_stats_to_tensorboard(stats:dict, writer:tensorboard.SummaryWriter,
